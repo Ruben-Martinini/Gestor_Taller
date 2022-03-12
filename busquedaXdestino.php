3c8ca3cd8b6924cade38b5e8f1050c7085e9925d
Ruben <rubariel78@gmail.com>
2022-03-12 18:23:46 -0300

    archivos incorporados al proyecto

---
+++ busquedaXdestino.php
@@ -0,0 +1,38 @@
+<?php
+   session_start();
+   if(!isset($_SESSION['usuario'])){
+      header('location: index.php');
+      die();
+   }
+   $operacion="BUSCADOR";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Búsqueda de repuestos</title>
+     <!-- CSS only -->
+     
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+     
+     <link rel="stylesheet" href="Estilos/Styles.css?v=<?php echo time(); ?>"> 
+     <link rel="stylesheet" href="Estilos/Insert.css?v=<?php echo time(); ?>"> 
+     
+     
+     
+
+</head>
+<body>
+
+
+   <?php include 'plantillas/cabeza_cruds.php'; ?>
+   <?php include 'plantillas/form_buscar_ubicacion.php'; ?>
+   <?php include 'plantillas/pie.php'; ?>
+    
+  
+</body>
+</html>
---
+++ plantillas/form_lista_ubicacion.php
@@ -0,0 +1,66 @@
+<!-- Inicio de listado de resultados -->
+<section id="container-lista" class="pt-3 pb-3">
+      
+
+        <!--Categorias-->
+
+        <div class="row">
+          <div class="col text-uppercase text-center">
+          
+         
+            <p> Resultados</p>
+            
+          </div>
+        </div>
+        
+        <div class="row marco-lista">
+          <div class="col col-md-10 offset-md-1 col-lg-8 offset-lg-2 pt-2 marco-tabla">
+          
+          <table class="table">
+              <thead>
+                <tr>
+                  <th class="centrado" scope="col">id Repuesto</th>
+                  <th scope="col">N° de Parte</th>
+                  <th scope="col">Designación</th>
+                  <th scope="col">Marca</th>
+                  <th class="centrado" scope="col">DP1</th>
+                  <th class="centrado" scope="col">DP2</th>
+                  <th class="centrado" scope="col">DP3</th>
+                  <th class="centrado" scope="col">Editar</th>
+                  <th class="centrado" scope="col">Eliminar</th>
+                </tr>
+              </thead>
+              <tbody>
+              
+              <!--Inicio de bucle de extracción de registros de base de datos-->
+              <?php if($resultado){
+                
+                      while($tabla = $nuevaConexion->ObtenerFilas($resultado)){ ?>  
+              <tr>
+                  <th class="centrado" scope="row"><?php echo $tabla[0]; ?></th>
+                  
+                  <td><?php echo $tabla[1]; ?></td>
+                  <td><?php echo $tabla[2]; ?></td>
+                  <td><?php echo $tabla[3]; ?></td>
+                  <td class="centrado"><?php echo $tabla[4]; ?></td>
+                  <td class="centrado"><?php echo $tabla[5]; ?></td>
+                  <td class="centrado"><?php echo $tabla[6]; ?></td>
+                  <td class="centrado"><a href="edicion.php?id=<?php echo $tabla[0]; ?>" target="_SELF">
+                  <i class="far fa-edit"></i>
+                  </a></td>
+                  <td class="centrado"><a href="eliminar.php?id=<?php echo $tabla[0]; ?>" target="_SELF">
+                  <i class="far fa-trash-alt"></i>
+                  </a></td>
+              </tr>
+              <?php   }
+                    $nuevaConexion->LimpiarResultado($resultado); }?>
+
+              </tbody>
+            </table>
+
+
+          </div>
+        </div>
+      
+    </section>
+    <!-- fin de listado de resultados -->
