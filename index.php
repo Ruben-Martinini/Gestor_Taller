829c43b488e278456d8afb688a056614e93ed6fa
Ruben <rubariel78@gmail.com>
2022-03-12 18:11:37 -0300

    cambio de nombre de archivo a index

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Sitio oficial del Taller de la Dirección Nacional del antártico">
+    <title>Página principal gestor taller DNA</title>
+     <!-- CSS only -->
+     
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    
+     <link rel="stylesheet" href="Estilos/Styles.css?v=<?php echo time(); ?>">
+
+</head>
+<body>
+  
+   <?php include 'plantillas/cabeza.php'; ?>
+   <?php include 'plantillas/form_ingreso.php'; ?>
+   <?php include 'plantillas/pie.php'; ?>
+
+   <script src="js/index.js"></script>
+
+</body>
+</html>
